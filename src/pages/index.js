ae0129444e7eeb6c79631c92afcee90e7ad2c108
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-08-21 09:23:45 +0600

    Routes added and SOme Pages Were Created

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import Blogs from "./Blogs";
+import Contact from "./Contact";
+import Services from "./Services";
+import About from "./About";
+
+export default {
+    Home,
+    Blogs,
+    Contact,
+    Services,
+    About
+};

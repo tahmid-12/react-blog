d0d6c4823142d1214091a768a945498fbcd2e331
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-08-23 23:56:29 +0600

    Nabar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <>
-      <div>Navbar</div>
+      <Navbar />
       <h1 className='text-7xl'>Hello Blog</h1>
       <Outlet />
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+
+  const navItems = [
+    {path: "/", link: "Home"},
+    {path: "/services", link: "Services"},
+    {path: "/about", link: "About"},
+    {path: "/blogs", link: "Blogs"},
+    {path: "/contact", link: "Contact"}
+  ];
+
+  return (
+    <header className='bg-black'>
+      <nav className='px-4 py-4'>
+        <a href='/' className='text-xl font-bold text-white'>Design<span className='text-orange-500'>DK</span></a>
+
+        <ul className='md:flex gap-12 text-lg hidden'>
+          {
+            navItems.map(({path,link}) => <li className='text-white' key={path}>
+              <NavLink to={path}>{link}</NavLink>
+            </li>)
+          }
+        </ul>
+
+        <div>
+          <a href="/"></a>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar

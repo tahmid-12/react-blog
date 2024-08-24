7a776c544defcb0f19dd60c7debb04b47d1acdc5
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-08-24 18:10:31 +0600

    Navbar error handle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
       </nav>
 
       <div>
-        <ul className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${isMenuOpen ? "fixed top-0 left-0 w-full transition-all ease-out duration-150" : "hidden"}`}>
+        <ul className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 bg-white ${isMenuOpen ? "fixed w-full transition-all ease-out duration-150" : "hidden"}`}>
           {
             navItems.map(({path,link}) => <li className='text-black' key={path}>
               <NavLink onClick={toggleMenu} to={path}>{link}</NavLink>

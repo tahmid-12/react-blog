be0751ff45b815339730c9ff3f33c910f3def986
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-08-28 23:26:02 +0600

    Blogpage created

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,15 @@
 import Banner from "./Banner";
 import BlogCard from "./BlogCard";
 import BlogPage from "./BlogPage";
+import CategorySelector from "./CategorySelector";
 import Navbar from "./Navbar";
+import Pagination from "./Pagination";
 
 export default {
     Navbar,
     Banner,
     BlogPage,
-    BlogCard
+    BlogCard,
+    Pagination,
+    CategorySelector
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import BlogPage from '../components/BlogPage'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <BlogPage />
     </div>
   )
 }
